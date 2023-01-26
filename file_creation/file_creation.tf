6f2d6070767c68632c89d763462c1cf8de265ad5
mohit-singhal <43447130+mohit-singhal@users.noreply.github.com>
2023-01-27 01:36:02 +0530

    5

--- file_creation/file_creation.tf
+++ file_creation/file_creation.tf
@@ -1,4 +1,4 @@
 resource "local_file" "foo" {
   content  = "foo!"
-  filename = r"F:\Learning\Terraform\terraform_practice\file_creation.txt"
+  filename = "F:\\Learning\\Terraform\\terraform_practice\\file_creation.txt"
 }
