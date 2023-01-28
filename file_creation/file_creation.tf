ee5046d74f28f9077e31677c582eef1f0fefdc36
mohit-singhal <43447130+mohit-singhal@users.noreply.github.com>
2023-01-28 18:19:33 +0530

    first

--- file_creation/file_creation.tf
+++ file_creation/file_creation.tf
@@ -1,4 +1,10 @@
-resource "local_file" "foo" {
+resource "local_file" "file1" {
   content  = "Hello Mohit Singhal!"
-  filename = "F:\\Learning\\Terraform\\terraform_practice\\file_creation.txt"
+  filename = "F:\\Learning\\Terraform\\terraform_practice\\file_creation1.txt"
+}
+
+resource "random_pet" "pet1" {
+ prefix = "MR"
+ length = "1"
+ separator = "-"
 }
